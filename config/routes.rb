78e13255d2aa518efcf6ef5a5370d69f35a08ad7
Fourfingerz <SzeChan@192.168.1.11>
2014-06-13 19:48:39 -0400

    A Better Welcome Page

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
    root 'welcome#index'
     resources :articles
     resources :categories
+    resources :welcome
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
